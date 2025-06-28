9c2cc1258dff3fb8d8cf18bcf3644afabaa8af88
Anushree401 <anushree1606balaji@gmail.com>
2025-06-28 17:11:54 +0530

    More updates

--- app.py
+++ app.py
@@ -43,6 +43,9 @@ def index():
 
     return render_template("scanner_index.html", result=result, recon=recon)
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=80)  
